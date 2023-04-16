8a1e88613226450a9d24d4f82ff57e3154ad40a3
An <antp9254@gmail.com>
2023-04-16 10:51:52 +0700

    java

--- OOP Lab/Week_1/DaysOfMonth.java
+++ OOP Lab/Week_1/DaysOfMonth.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class DaysOfMonth {
     public static void main(String[] args) {
-        String[] months = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] months1 = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] months2 = {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        String[] months3 = {"", "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."};
+        String[] months4 = {"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
         Scanner sc = new Scanner(System.in);
         boolean checkMonth = true, checkYear = true;
-        int month = 0, year = 0, day = 0;
+        int month = 0, year = 0;
         while (checkMonth) {
             System.out.print("Please enter month: ");
             String iMonth = sc.nextLine();
-            if (!check(iMonth)) {
-                System.out.println("Invalid!");
-            } else {
-                if (iMonth.length() != 1) {
-                    for (int i=1; i<=12; i++) {
-                        if (iMonth.substring(0,3).equals(months[i].substring(0,3))) {
-                            month = i;
-                        }
-                    }
+            boolean check = true;
+            for (int i=1; i<=12; i++) {
+                if (iMonth.equals(months1[i]) || iMonth.equals(months2[i]) || iMonth.equals(months3[i]) || iMonth.equals(months4[i])) {
+                    checkMonth = false;
+                    month = i;
+                    break;
                 }
-                else month = Integer.parseInt(iMonth);
-                checkMonth = false;
+            }
+            if (!check) {
+                System.out.println("Invalid");
             }
         }
         while (checkYear) {
@@ -38,6 +39,7 @@ public class DaysOfMonth {
                 if ((year % 4 == 0 && year % 100 !=0) || year % 400 == 0)  {
                     System.out.println("Number of days: 29");
                 } else System.out.println("Number of days: 28");
+                break;
             case 1:
             case 3:
             case 5:
@@ -51,22 +53,5 @@ public class DaysOfMonth {
                 System.out.println("Number of days: 30");
                 break;
         }
-
-
     }
-
-    private static boolean check(String month) {
-        String[] months = {"","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        for (int i = 1; i <= 12; i++) {
-            if (!(month.equals(months[i])) || !(month.equals(months[i] + ".")) || !(month.equals(""))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
-
-
 }
