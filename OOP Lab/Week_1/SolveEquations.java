4dc9e3c414afecc35a32f0b361f2a30a92067ffd
An <antp9254@gmail.com>
2023-04-16 11:58:25 +0700

    java

--- OOP Lab/Week_1/SolveEquations.java
+++ OOP Lab/Week_1/SolveEquations.java
@@ -6,15 +6,14 @@ public class SolveEquations {
         do {
             System.out.println("1. Solve the first-degree equation with one varible");
             System.out.println("2. Solve the system of first-degree equations with two variable");
+            System.out.println("3. Solve the second-degree equation with one variable");
             System.out.println("0. Exit");
             System.out.println("Please choose: ");
             choice = sc.nextInt();
             if (choice == 1) {
                 double a, b;
-                System.out.println("Please enter a: ");
-                a = sc.nextDouble();
-                System.out.println("Please enter b: ");
-                b = sc.nextDouble();
+                System.out.println("Please enter a and b: ");
+                a = sc.nextDouble(); b = sc.nextDouble();
                 if (a == 0) {
                     System.out.println("The equation have no solution");
                 } else {
@@ -39,6 +38,25 @@ public class SolveEquations {
                 } else {
                     System.out.println("The solution of this system equation: " + (D1 / D) + " and " + (D2 / D));
                 }
+            } else if (choice == 3) {
+                double a, b, c;
+                System.out.println("Please enter 3 parameters: ");
+                a = sc.nextDouble(); b = sc.nextDouble(); c = sc.nextDouble();
+                while (a==0) {
+                    System.out.println("Invalid a. Please enter again: ");
+                    a = sc.nextDouble();
+                }
+                double delta = b*b - 4*a*c;
+                if (delta < 0) {
+                    System.out.println("The equation has no solution");
+                } else if (delta == 0) {
+                    System.out.println("The equation has one solution: "+ -b/(2*a));
+                } else {
+                    double x1= (-b + Math.sqrt(delta))/(4*a);
+                    double x2 = (-b - Math.sqrt(delta))/(4*a);
+                    System.out.println("The equation has two solution: "+ x1 + " and " + x2);
+                }
+
             } else {
                 System.out.println("Invalid! Please enter again!");
             }
