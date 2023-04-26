a293068cf2fd579711d0b1585ff23f5c6ba14274
An <antp9254@gmail.com>
2023-04-26 16:54:44 +0700

    Revert "Revert "java""
    This reverts commit 54c7401d3f0302828609e01741a4715ec6d1c2e2.

---
+++ OOP/Example/Lesson4/Example.java
@@ -0,0 +1,13 @@
+package Lesson4;
+
+public class Example {
+    int i;
+
+
+    public static void main(String[] args) {
+        Example ex = new Example();
+        System.out.println(ex.i);
+//        int j;
+//        System.out.println(j);
+    }
+}
