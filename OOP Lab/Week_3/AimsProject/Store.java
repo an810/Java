cdbe395011021b80915e4936878519be5d1b10b7
An <antp9254@gmail.com>
2023-05-13 21:18:31 +0700

    java

---
+++ OOP Lab/Week_3/AimsProject/Store.java
@@ -0,0 +1,43 @@
+package Week_3.AimsProject;
+
+public class Store {
+    public static final int MAX_ITEMS_IN_STORE = 200;
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAX_ITEMS_IN_STORE];
+    private int qtyInStore = 0;
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore == MAX_ITEMS_IN_STORE) {
+            System.out.println("The store is full");
+        } else {
+            itemsInStore[qtyInStore] = dvd;
+            qtyInStore++;
+        }
+    }
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean check = false;
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == dvd) {
+                for (int j = i+1; j < qtyInStore; j++) {
+                    itemsInStore[j-1] = itemsInStore[j];
+                };
+                qtyInStore--;
+                check = true;
+                System.out.println("The disc has been removed");
+                break;
+            }
+        }
+        if (!check) {
+            System.out.println("The disc has not been found");
+        }
+    }
+
+    public void print() {
+        System.out.println("***********************STORE**********************");
+        System.out.println("Items in store:");
+        for (int i = 0; i < qtyInStore; i++) {
+            System.out.println(itemsInStore[i].toString());
+        }
+        System.out.println("**************************************************");
+    }
+
+
+}
