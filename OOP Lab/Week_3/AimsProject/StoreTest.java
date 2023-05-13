0f94764b6c83b53fe796b6aaae61ddea3a994ec1
An <antp9254@gmail.com>
2023-05-13 21:21:22 +0700

    java

--- OOP Lab/Week_3/AimsProject/StoreTest.java
+++ OOP Lab/Week_3/AimsProject/StoreTest.java
@@ -9,7 +9,9 @@ public class StoreTest {
         store.addDVD(dvd2);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 19.95f);
         store.addDVD(dvd3);
-
+        store.print();
+        store.removeDVD(dvd1);
+        store.print();
 
     }
 }
